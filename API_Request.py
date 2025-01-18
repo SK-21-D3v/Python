3bb6f281f16e9bae7876c9419f077cd89ac910df
Sonia Kolay <sonia210804@gmail.com>
2025-01-18 11:44:45 +0530

    Update API_Request.py
    Made changes to the URL

--- API_Request.py
+++ API_Request.py
@@ -1,13 +1,14 @@
-# Fetch data from a public API and display information
 import requests
 
 def fetch_api_data():
-    url = "https://jsonplaceholder.typicode.com/posts/1"
+    url = "https://randomuser.me/api/"
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        print("Title:", data['title'])
-        print("Body:", data['body'])
+        user = data['results'][0]
+        print("Name:", user['name']['first'], user['name']['last'])
+        print("Gender:", user['gender'])
+        print("Email:", user['email'])
     else:
         print("Failed to fetch data. Status code:", response.status_code)
 
